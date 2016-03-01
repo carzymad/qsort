c23ecd5a018b072e5e171a7747a2e4734cea6aae
carzymad <2116913961@qq.com>
2016-03-02 00:01:54 +0800

    快排模板.1
    第一次完整得写出快排

---
+++ 快排.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+template<typename T>
+void qsort(T arry[], int begin, int end);
+
+int main()
+{
+    int length;
+    cout << "输入长度:";
+    cin >> length;
+    char *arry = new char[length];
+    srand(time(0));
+    for (int i = 0; i < length; i++)
+        //cin >> arry[i];
+        arry[i] = rand() % 26 + 97;
+
+    qsort(arry, 0, length-1);
+    //cout << (int)'a' << endl;
+    for (int i = 0; i < length; i++)
+        cout << arry[i] << ' ';
+    cout << endl;
+
+    return 0;
+}
+
+template<typename T>
+void qsort(T arry[], int begin, int end)
+{
+    if (begin >= end)
+        return;
+    int b = begin-1;
+    int flag = 0;
+    for(int i = begin; i <= end; i++)
+    {
+        if (arry[i] <= arry[end])
+        {
+            b++;
+            T tmp = arry[i];
+            arry[i] = arry[b];
+            arry[b] = tmp;
+
+        }
+    }
+    qsort(arry, begin, b-1);
+    qsort(arry, b+1, end);
+}
